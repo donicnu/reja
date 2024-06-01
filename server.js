d1f64b3053026131c9dff6e8fd3da1044f3dad51
donicnu <donicnu@gmail.com>
2024-06-02 03:30:13 +0900

    fix: changing port to 7011

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 const http = require("http");
-const mongodb = require("mongodb")
+const mongodb = require("mongodb");
 
 let db;
-const connectionString = "mongodb+srv://donicnu:9402898aaA@cluster0.hbnwx9r.mongodb.net/Reja"
+const connectionString =
+  "mongodb+srv://donicnu:9402898aaA@cluster0.hbnwx9r.mongodb.net/Reja";
 
-
-mongodb.connect(connectionString, {
+mongodb.connect(
+  connectionString,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}, (err, client) => {
-if(err) console.log("ERROR on connection MongoDB");
-else {
-    console.log("MongoDB connection successful");
-    // console.log(client);
-   module.exports = client;
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection successful");
+      // console.log(client);
+      module.exports = client;
 
-    const app = require("./app")
-    const server = http.createServer(app);
-    let PORT = 3000;
-    server.listen(PORT, function () {
-        console.log(`This server is running successfully on port:${PORT}, http://localhost:${PORT}`);
-    });
-}
-});
-
-
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 7011;
+      server.listen(PORT, function () {
+        console.log(
+          `This server is running successfully on port:${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
